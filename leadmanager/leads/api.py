c1d3131f361f62b36061d50f4687be24cb6406f3
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-04 19:52:47 +0300

    Inital API

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+from .models import Lead
+
+# Lead Viewset
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permissions_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
