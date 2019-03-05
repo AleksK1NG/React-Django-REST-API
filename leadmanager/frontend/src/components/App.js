d20b1e4a2b4ce44a391a11f8d78461f18bee20f7
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-05 14:00:48 +0300

    frontend init

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>JS</h1>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
